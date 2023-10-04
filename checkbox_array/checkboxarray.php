24704bbcd1447e33d5630820aae1c9ee75fc4a42
kevinbogonko <61062173+kevinbogonko@users.noreply.github.com>
2023-10-04 13:16:36 +0000

    added checkbox array functionality

---
+++ checkbox_array/checkboxarray.php
@@ -0,0 +1,9 @@
+
+<form method='post' id='userform' action='thisform.php'> <tr>
+    <td>Trouble Type</td>
+    <td>
+    <input type='checkbox' name='checkboxvar[]' value='Option One'>1<br>
+    <input type='checkbox' name='checkboxvar[]' value='Option Two'>2<br>
+    <input type='checkbox' name='checkboxvar[]' value='Option Three'>3
+    </td> </tr> </table> <input type='submit' class='buttons'> </form>
+
---
+++ checkbox_array/thisform.php
@@ -0,0 +1,7 @@
+<?php 
+echo 'pressed'; 
+if (isset($_POST['checkboxvar'])) 
+{
+     print_r($_POST['checkboxvar']); 
+}
+?>
