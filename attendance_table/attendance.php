ec0cb441224a59fa901eec5e3899653d6843df3e
kevinbogonko <61062173+kevinbogonko@users.noreply.github.com>
2023-10-17 10:54:46 +0000

    too tricky to solve this today

--- attendance_table/attendance.php
+++ attendance_table/attendance.php
@@ -8,14 +8,39 @@
 
     //get column names out of date values
 
-    $day1 = date("d-m-y");
-    $day2 = date();
-    $day3 = date();
-    $day4 = date();
-    $day5 = date();
-    $day6 = date();
-    $day7 = date();
+    // lets calculate leap year
 
-    echo $day1;
+    //leap years = last two digits are divisible by 4 \
+
+    $year_array =  array();
+    $get_year = date("y");
+    
+    print_r($get_year);
+
+    $result = $get_year % 4; 
+
+    if(!$result){
+        echo 'leap year';
+    } else {
+        echo 'not leap year';
+    }
+
+    // $day1 = date("d-m-y");
+    // $day2 = date();
+    // $day3 = date();
+    // $day4 = date();
+    // $day5 = date();
+    // $day6 = date();
+    // $day7 = date();
+
+    // echo $day1;
+
+    // get February month of leap year
+
+    
+    // get February month of not leap year
+
+
+
 
 ?>
